b548858b95f50116fb572c15145a5bf1922e374a
Urcodebudy <98595461+Urcodebudy@users.noreply.github.com>
2022-02-18 00:28:06 +0530

    delete old files

--- result-manager/back-end/controllers/student.js
+++
@@ -1,6 +0,0 @@
-exports.testRoute = (req, res, next) => {
-    res.status(200).json({
-        success : true,
-        msg : "This is testing route "
-    })
-}
--- result-manager/back-end/routes/student.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {testRoute} = require('../controllers/student');
-
-router.route('/test').get(testRoute);
-
-module.exports = router;
